b978b7a841ea5b3ad5ba600a06800430a51d9d25
Mathew <apps@rocketpoweredsolutions.com>
2019-10-26 16:37:17 +0800

    initial commit

---
+++ src/Controller/Component/ConfigReaderComponent.php
@@ -0,0 +1,39 @@
+<?php
+namespace FloodgateCake\Controller\Component;
+
+use Cake\Controller\Component;
+use FloodgateSDK\FloodgateClient;
+
+class ConfigReaderComponent extends Component
+{
+      
+
+    public function initialize(Array $config)
+    {
+      parent::initialize($config);
+      debug('config');
+
+      
+
+      // App::import('Vendor', 'FloodgateCake.FloodgateSDK/filename');
+
+      $user = new \FloodgateSDK\User('id');
+      $client = new FloodgateClient($sdkkey);
+      // $client = new \FloodgateCake\FloodgateSDK\FloodgateClient\FloodgateClient($sdkkey);
+
+      
+
+      debug($client);
+    }
+
+    public function doComplexOperation($amount1, $amount2)
+    {
+      debug($this);
+      return $amount1 + $amount2;
+    }
+
+    public function GetValue($key, $defaultValue, $user = [])
+    {
+
+    }
+}
---
+++ src/Plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FloodgateCake;
+
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+use Cake\Core\Configure;
+use Cake\Utility\Hash;
+
+/**
+ * Plugin for FloodgateCake
+ */
+class Plugin extends BasePlugin
+{
+  public $client;
+
+  public function bootstrap(PluginApplicationInterface $app)
+  {
+    // Add constants, load configuration defaults.
+    // By default will load `config/bootstrap.php` in the plugin.
+    parent::bootstrap($app);
+    
+    $config = (array)Configure::read('Floodgate');
+    $sdkkey = Hash::get($config, 'sdkkey');
+    if (!$sdkkey) {
+      throw new InvalidArgumentException('Floodgate SDK Key not provided.');
+    }
+
+
+  }
+}
