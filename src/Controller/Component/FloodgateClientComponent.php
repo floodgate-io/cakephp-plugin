f87c49bdf8c6a4dfd8282e887b3f56f1c92b240d
Mathew <apps@rocketpoweredsolutions.com>
2019-11-13 07:31:10 +0800

    updated exception handling

--- src/Controller/Component/FloodgateClientComponent.php
+++ src/Controller/Component/FloodgateClientComponent.php
@@ -10,6 +10,8 @@ class FloodgateClientComponent extends Component
 {
   private $client;
 
+  private $isReady = false;
+
   public function initialize(Array $config)
   {
     parent::initialize($config);
@@ -21,29 +23,39 @@ class FloodgateClientComponent extends Component
       throw new \InvalidArgumentException('Floodgate SDK Key not provided.');
     }
 
-    if (empty($config)) {
-      $this->client = new FloodgateClient($sdkkey);
+    try {
+      if (empty($config)) {
+        $this->client = new FloodgateClient($sdkkey);
+      }
+      else {
+        $clientconfig['timeout'] = 10;
+  
+        if (!empty($config['timeout'])) {
+          $clientconfig['timeout'] = $config['timeout'];
+        }
+  
+        if (!empty($config['logpath'])) {
+          $clientconfig['logger'] = new \FloodgateSDK\Loggers\FileLogger($config['logpath']);
+        }
+  
+        // new \FloodgateSDK\Cache\FloodgateCache(new \FloodgateSDK\Cache\ArrayCache())
+        $clientconfig['cache'] = new \FloodgateSDK\Cache\FloodgateCache(\Cake\Cache\Cache::pool("default"));
+  
+        $this->client = new FloodgateClient($sdkkey, $clientconfig);
+
+        $this->isReady = true;
+      }
     }
-    else {
-      $clientconfig['timeout'] = 10;
-
-      if (!empty($config['timeout'])) {
-        $clientconfig['timeout'] = $config['timeout'];
-      }
-
-      if (!empty($config['logpath'])) {
-        $clientconfig['logger'] = new \FloodgateSDK\Loggers\FileLogger($config['logpath']);
-      }
-
-      // new \FloodgateSDK\Cache\FloodgateCache(new \FloodgateSDK\Cache\ArrayCache())
-      $clientconfig['cache'] = new \FloodgateSDK\Cache\FloodgateCache(\Cake\Cache\Cache::pool("default"));
-
-      $this->client = new FloodgateClient($sdkkey, $clientconfig);
+    catch(Exception $e) {
+      $this->isReady = false;
     }
   }
 
   public function GetValue($key, $defaultValue, $user = null)
   {
+    if (!$this->isReady)
+      return $defaultValue;
+    
     if (empty($user)) {
       return $this->client->GetValue($key, $defaultValue);
     }
